80ecdc7b98b92ac1c338712f84e2e2527644caaf
Melanie <melanie.rogan@ft.com>
2020-09-19 22:00:38 +0100

    readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mork-up!
+
+Upload an image of your hospital appointment print-out and the app will create a `.ics` file that you can add to your calendar. 
+
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const optiic = new (require('optiic'))({
-	apiKey: 'api_test_key', // Not required, but having one removes limits (get your key at https://optiic.dev).
+	apiKey: 'api_adedc6b7-f043-45ce-b718-5701311741da', // Not required, but having one removes limits (get your key at https://optiic.dev).
 });
 
 let options = {
-	image: 'https://rolfscountryhouse.com/wp-content/uploads/2015/11/rolfs-menu-1.jpg', // url of the image
+	image: './images/test4.JPG', // url of the image
 	mode: 'ocr', // ocr
 };
 
