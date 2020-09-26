2c59fe95867dc05b24de1254b456113e3f70732f
Melanie <melanie.rogan@ft.com>
2020-09-26 19:14:36 +0100

    getting express up and running to pass things along to the fronted for the upload form

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const { createEvent } = require('./createEvent');
-createEvent();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+app.set('view engine', '.html');
+app.set('views', __dirname + '/views');
+const handlebarsInstance = exphbs.create({
+	extname: '.html',
+	defaultLayout: 'main',
+});
+
+app.engine('html', handlebarsInstance.engine);
+
+app.get('/', (req, res) => {
+	res.render('index.html');
+});
+
+// createEvent();
+
+const PORT = process.env.PORT || 0;
+const server = app.listen(PORT, () => {
+	console.log(`app listening at http://localhost:${server.address().port})`);
+});
+
+server;
